26b635390527d691f2c82bdf4f2064c95f45eb2c
Taylor Sallee <tsallee@mines.edu>
2013-11-10 22:54:39 -0700

    accidentally deleted push_to_luna.sh

--- crowdsource/push_to_luna.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-tar -cvzf crowdsource.tar.gz src
-scp crowdsource.tar.gz luna.mines.edu:/srv/csci_445/2013_fall/team10
-ssh luna.mines.edu 'tar -xvzf /srv/csci_445/2013_fall/team10/crowdsource.tar.gz'
-ssh luna.mines.edu 'rm -f /srv/csci_445/2013_fall/team10/crowdsource.tar.gz'
-#rm -f crowdsource.tar.gz
--- push_to_luna.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-tar -cvzf crowdsource.tar.gz crowdsource
-scp crowdsource.tar.gz luna.mines.edu:/srv/csci_445/2013_fall/team10
-ssh luna.mines.edu 'tar -xvzf /srv/csci_445/2013_fall/team10/crowdsource.tar.gz -C /srv/csci_445/2013_fall/team10; rm -f /srv/csci_445/2013_fall/team10/crowdsource.tar.gz'
-rm -f crowdsource.tar.gz
-firefox http://luna.mines.edu/csci_445/2013_fall/team10/crowdsource/
