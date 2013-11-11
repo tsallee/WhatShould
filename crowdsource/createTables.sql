d26b4a9584d1a69d1790d7cec49e7091845d09a8
Kameron W. Kincade <kkincade@bb136-13.mines.edu>
2013-11-11 00:08:42 -0700

    Rough take at createUserAccount.php

--- crowdsource/createTables.sql
+++ crowdsource/createTables.sql
@@ -2,22 +2,23 @@ USE team10;
 
 CREATE TABLE user(
 	id int unsigned not null auto_increment primary key,
-	username char(20) not null,
-	password char(20) not null,
-	email char(50) not null,
+	username varchar(255) not null, 
+	password varchar(255) not null,
+	email varchar(255) not null,
 	score int not null,
 	currency int not null,
 	quality_count int not null,
-	new_count int not null
+	new_count int not null,
+	unique (username) /* This may be a problem */
 );
 
 CREATE TABLE post(
 	id int unsigned not null auto_increment primary key,
-	content char(140) not null,
+	content varchar(255) not null,
 	upvotes int not null,
 	downvotes int not null,
 	foreign key (user_id) references user(id),
-	category char(4) not null check (category = "day" || category = "year" || category = "life"),
+	category varchar(255) not null check (category = "day" || category = "year" || category = "life"),
 	flagged boolean not null
 );
 
--- crowdsource/createUserAccount.php
+++ crowdsource/createUserAccount.php
@@ -1,11 +1,37 @@
 <?php
-@ $db = new mysqli('localhost', 'team10', 'pear', 'team10');
 
+// Try to connect to the database
+@ $db = new mysqli('localhost', 'team10', 'pear', 'team10');
 if (mysqli_connect_errno()) {
 	echo 'Error: Could not connect to database. Please try again later.';
 	exit;
 }
 
+// Get field information from Create Account page
+$username = $_POST('username');
+$password = $_POST('password');
+$email = $_POST('email');
 
+// Prepare query statement and execute it
+$create_user_query = "INSERT INTO user (id, username, password, email, score, currency, quality_count, new_count) VALUES (?, ?, ?, ?, ?, ?, ?, ?)"
+$stmt = $db->prepare($create_user_query);
+$stmt->bind_param(NULL, $username, $password, $email, 0, 20, 0, 0);
+$stmt->bind_result($result);
+$stmt->execute();
+
+//Fetches result and puts it in $result
+$stmt->fetch();
+
+// Free the result and close database
+$stmt->free_result();
+$db->close();
+
+if (!$result) {
+	// Result was false (error inserting)
+	echo "Error: Could not insert user into the database. Please try again later.";
+	exit
+} else {
+	echo "Yay! Thanks for joining";
+}
 
 ?>
