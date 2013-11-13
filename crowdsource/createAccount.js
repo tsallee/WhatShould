74c3c358831d106a2c5ab04c5004f4c052083086
Taylor Sallee <tsallee@mines.edu>
2013-11-12 17:07:29 -0700

    Added create account page

--- crowdsource/createAccount.js
+++ crowdsource/createAccount.js
@@ -1,3 +1,11 @@
-function load() {
-	alert("hello");
+function checkFields() {
+	var password = document.getElementById("password");
+	var confirmPassword = document.getElementById("confirm_password");
+	if ( password.value != confirmPassword.value ) {
+		var passErrDiv = document.getElementById("no_pass_match");
+		$(passErrDiv).slideDown(600);
+		setTimeout(function() { $(passErrDiv).slideToggle(600); }, 3000);
+		password.value = "";
+		confirmPassword.value = "";
+	}
 }
