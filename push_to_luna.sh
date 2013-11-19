b04ecd3caa5fc6590e396ee2a21cbc9d8c1aacce
Taylor Sallee <tsallee@mines.edu>
2013-11-19 16:31:43 -0700

    removed firefox start from push script

--- push_to_luna.sh
+++ push_to_luna.sh
@@ -3,4 +3,4 @@ tar -cvzf crowdsource.tar.gz crowdsource
 scp crowdsource.tar.gz luna.mines.edu:/srv/csci_445/2013_fall/team10
 ssh luna.mines.edu 'chmod -R 777 /srv/csci_445/2013_fall/team10/crowdsource; rm -r /srv/csci_445/2013_fall/team10/crowdsource; tar -xvzf /srv/csci_445/2013_fall/team10/crowdsource.tar.gz -C /srv/csci_445/2013_fall/team10; chmod -R 777 /srv/csci_445/2013_fall/team10/crowdsource; rm -f /srv/csci_445/2013_fall/team10/crowdsource.tar.gz'
 rm -f crowdsource.tar.gz
-firefox http://luna.mines.edu/csci_445/2013_fall/team10/crowdsource/
+# firefox http://luna.mines.edu/csci_445/2013_fall/team10/crowdsource/
